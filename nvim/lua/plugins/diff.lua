62518bd0f7babfba3985567872a9d69565731890
caramel-sau <washing4563@gmail.com>
2025-12-07 15:00:46 +0900

    feat: diffview にキーマップを追加

--- nvim/lua/plugins/diff.lua
+++ nvim/lua/plugins/diff.lua
@@ -1,4 +1,8 @@
 return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
+  keys = {
+    { "<leader>hd", "<cmd>DiffviewOpen HEAD~1<CR>", desc = "DiffviewOpen" },
+    { "<leader>hf", "<cmd>DiffviewFileHistory %<CR>", desc = "DiffviewFileHistory" },
+  }
 }
