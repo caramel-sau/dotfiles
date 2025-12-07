875b370223597a05af80d24566c9d9c72d693237
caramel-sau <washing4563@gmail.com>
2025-12-07 19:24:56 +0900

    feat: telescope で隠しフォルダとファイルを検索対象に追加

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,10 +1,49 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope find_files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Telescope live_grep" },
-    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Telescope buffers" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden"
+          },
+          file_ignore_patterns = { "node_modules/", ".git/" },
+          find_files = {
+            hidden = true,
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- the default case_mode is "smart_case"
+          }
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep hidden=true<cr>",  desc = "Live Grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",                desc = "Find Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>",              desc = "Help Tags" },
+    },
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    event = { "VeryLazy" },
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
   },
 }
